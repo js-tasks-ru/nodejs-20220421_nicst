e3f30992de6077266678a05bac725074bb09543e
Nikolai <nicst@mail.ru>
2022-05-04 02:07:43 +0400

    Stream with limit

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,19 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
-  constructor(options) {
-    super(options);
+  constructor({limit, encoding}) {
+    super({limit, encoding});
+    this._limit = limit;
+    this._currentLength = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    if (this._limit < chunk.length + this._currentLength) {
+      callback(new LimitExceededError());
+    } else {
+      this._currentLength += chunk.length;
+      callback(null, chunk);
+    }
   }
 }
 
