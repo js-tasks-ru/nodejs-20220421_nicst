87eb7daabb8a0516fd74101645a972f5c6bfd21d
Nikolai <nicst@mail.ru>
2022-05-06 01:01:52 +0400

    Add file-server

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,18 +1,35 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
 
 const server = new http.Server();
+const fs = require('fs');
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
-
   switch (req.method) {
     case 'GET':
-
+      if (! /^[^\/]*$/.test(pathname)) {
+        console.log(pathname);
+        res.statusCode = 400;
+        res.end('Bad request');
+        return;
+      }
+      fs.readFile(filepath, (err, data) => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('File not found');
+          } else {
+            res.statusCode = 500;
+            res.end(`Internal server error:\n${err.toString()}`);
+          }
+        } else {
+          res.end(data);
+        }
+      });
       break;
 
     default:
