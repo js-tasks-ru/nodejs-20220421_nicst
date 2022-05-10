09780581d91bba1e9eb9c022eb83da95466db4ab
Nikolai <nicst@mail.ru>
2022-05-11 02:55:59 +0400

    Add new tests, fix validator

--- 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
+++ 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
@@ -3,6 +3,35 @@ const expect = require('chai').expect;
 
 describe('testing-configuration-logging/unit-tests', () => {
   describe('Validator', () => {
+    it('валидатор проверяет, что передан объект', () => {
+      const validator = new Validator({ });
+
+      const errors = validator.validate(null);
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('-');
+      expect(errors[0]).to.have.property('error')
+          .and.to.be.equal('expected not empty object, got null');
+    });
+
+    it('валидатор проверяет тип поля', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string',
+          min: 5,
+          max: 10,
+        },
+      });
+
+      const errors = validator.validate({
+        name: false,
+      });
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('name');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('expect string, got boolean');
+    });
+
     it('валидатор проверяет строковые поля', () => {
       const validator = new Validator({
         name: {
@@ -12,11 +41,27 @@ describe('testing-configuration-logging/unit-tests', () => {
         },
       });
 
-      const errors = validator.validate({ name: 'Lalala' });
+      const errors = validator.validate({name: 'Lalala'});
 
       expect(errors).to.have.length(1);
       expect(errors[0]).to.have.property('field').and.to.be.equal('name');
       expect(errors[0]).to.have.property('error').and.to.be.equal('too short, expect 10, got 6');
+    });
+
+    it('валидатор проверяет числовые поля', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 10,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({age: 6});
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('too little, expect 10, got 6');
     });
   });
 });
