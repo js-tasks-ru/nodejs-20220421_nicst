82cb1aad29f5604458f38463499004e99a4c98b8
Nikolai <nicst@mail.ru>
2022-05-23 01:49:17 +0400

    Add Category and Product models

--- 06-mongodb-mongoose/01-schema-model/models/Product.js
+++ 06-mongodb-mongoose/01-schema-model/models/Product.js
@@ -1,7 +1,29 @@
-const mongoose = require('mongoose');
+const {Schema} = require('mongoose');
 const connection = require('../libs/connection');
 
-const productSchema = new mongoose.Schema({
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  subcategory: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  images: [String],
 });
 
 module.exports = connection.model('Product', productSchema);
