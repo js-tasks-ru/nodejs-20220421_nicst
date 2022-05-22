f9d81b9e961fad56ef49df4dba736471b70574d0
Nikolai <nicst@mail.ru>
2022-05-23 03:18:48 +0400

    Implement rest-api

--- 06-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,8 @@
+const Category = require('../models/Category');
+const mapCategory = require('../mappers/category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find();
+  console.log(categories);
+  ctx.body = {categories: categories.map(mapCategory)};
 };
