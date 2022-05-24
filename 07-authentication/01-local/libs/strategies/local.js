15b4ca5a458424efafc5bc761a1303d47d3f6a18
Nikolai <nicst@mail.ru>
2022-05-25 02:21:25 +0400

    Implement local strategy

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,23 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
+
+const INCORRECT_PASSWORD = 'Неверный пароль';
+const USER_DOESNOT_EXIST = 'Нет такого пользователя';
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async function(email, password, done) {
+      const user = await User.findOne({
+        email,
+      });
+      if (!user) {
+        done(null, false, USER_DOESNOT_EXIST);
+        return;
+      }
+      if (!await user.checkPassword(password)) {
+        done(null, false, INCORRECT_PASSWORD);
+      } else {
+        done(null, user);
+      }
     },
 );
